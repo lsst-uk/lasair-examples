d7e41a2ec3bc9100aeaa4abf3672293ec43bfae7
Roy Williams <roy@roe.ac.uk>
2025-10-27 09:24:37 +0000

    bazinblackbody

---
+++ notebooks/features/6_bazinBlackBody/settings_template.py
@@ -0,0 +1,5 @@
+# copy this file to settings.py, 
+# log in to Lasair, click your username at top right then "profile"
+# copy the API Token in place of the xxxxxxx
+API_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx''
+
