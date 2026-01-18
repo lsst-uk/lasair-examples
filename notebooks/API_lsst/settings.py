6ad821bf989e64e3244b5d26d56de6a131274a3c
Roy Williams <roy@roe.ac.uk>
2026-01-18 16:13:07 +0000

    update

---
+++ notebooks/API_lsst/settings.py
@@ -0,0 +1,6 @@
+
+# su on lasair-lsst-dev
+#API_TOKEN = '73f19bafabb81d182d7346f063be69a0e9315d83'
+
+# su on lasair-lsst
+API_TOKEN = 'b5fdcda4b8416f95466147c2514422589b7a3693'
--- notebooks/features/6_bazinBlackBody/settings_template.py
+++
@@ -1,5 +0,0 @@
-# copy this file to settings.py, 
-# log in to Lasair, click your username at top right then "profile"
-# copy the API Token in place of the xxxxxxx
-API_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx''
-
